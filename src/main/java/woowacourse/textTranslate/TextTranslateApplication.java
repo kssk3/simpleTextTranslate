ed81258f219226c97643023d6a32069d60124d37
kss8014 <kss8014@daum.net>
2025-11-10 15:28:48 +0900

    docs : README.md 작성 간단한 텍스트 번역 프로그램

---
+++ src/main/java/woowacourse/textTranslate/TextTranslateApplication.java
@@ -0,0 +1,13 @@
+package woowacourse.textTranslate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextTranslateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextTranslateApplication.class, args);
+	}
+
+}
